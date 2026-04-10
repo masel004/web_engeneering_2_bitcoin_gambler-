f8c5f8b3dc7310a6e411ca0991e6d32829107467
masel004 <boehmel.marcel@gmail.com>
2026-04-10 11:33:47 +0200

    First Commit

---
+++ src/main/java/de/dhbwravensburg/webeng/bitcoin_gambler/BitcoinGamblerApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng.bitcoin_gambler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinGamblerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinGamblerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbwravensburg/webeng/bitcoin_gambler/BitcoinGamblerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng.bitcoin_gambler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinGamblerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
